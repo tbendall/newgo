da55a1b324ca0ee68fad4b533cb4dee9f920d158
TristanB <tristan@bendall.cow>
2025-05-21 15:32:26 +0000

    more go learning

--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -1,16 +1,25 @@
 package main
 
 import (
-    "net/http"
+	"fmt"
 
-    "github.com/gin-gonic/gin"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-    router := gin.Default()
-    router.GET("/albums", getAlbums)
 
-    router.Run("localhost:8080")
+	var nums = []string{"a", "b", "c"}
+
+	fmt.Println(len(nums))
+
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+	router.POST("/albums", postAlbums)
+
+	router.Run("localhost:8080")
+
 }
 
 type album struct {
@@ -20,16 +29,32 @@ type album struct {
 	Price  float64 `json:"price"`
 }
 
-var albums []album {
+var albums = []album{
 
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-    {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+func getAlbums(c *gin.Context) {
+
+	c.IndentedJSON(http.StatusOK, albums)
 
 }
 
-func getAlbums (c *gin.Context) {
+func postAlbums(c *gin.Context) {
 
-	c.IndentedJSON(http.StatusOK, albums)
+	var newAlbum album
+
+	// Call BindJSON to bind the received JSON to
+	// newAlbum.
+	if err := c.BindJSON(&newAlbum); err != nil {
+
+		return
+	}
+
+	albums = append(albums, newAlbum)
+
+	c.IndentedJSON(http.StatusCreated, newAlbum)
 
 }
